c23ddf8feafd71d81d90206c3913600f744aec7a
Drew Conner <drew@connermail.com>
2016-04-14 10:33:17 -0400

    Move to Framework Architecture

---
+++ DCKeychainItemWrapper/KeychainItemWrapper.h
@@ -0,0 +1,20 @@
+//
+//  DCKeychainItemWrapper.h
+//  DCKeychainItemWrapper
+//
+//  Created by Drew Conner on 4/14/16.
+//  Copyright © 2016 Drew Conner. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DCKeychainItemWrapper.
+FOUNDATION_EXPORT double DCKeychainItemWrapperVersionNumber;
+
+//! Project version string for DCKeychainItemWrapper.
+FOUNDATION_EXPORT const unsigned char DCKeychainItemWrapperVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DCKeychainItemWrapper/PublicHeader.h>
+
+#import <DCKeychainItemWrapper/DCKeychainItemWrapper.h>
+
