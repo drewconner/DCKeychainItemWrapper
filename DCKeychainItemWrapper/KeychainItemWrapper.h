2be55aec9e57cd35da35844725833196faf8fdd6
Drew Conner <drew@connermail.com>
2016-04-18 08:31:00 -0400

    Fix typo

--- DCKeychainItemWrapper/KeychainItemWrapper.h
+++ DCKeychainItemWrapper/KeychainItemWrapper.h
@@ -16,5 +16,5 @@ FOUNDATION_EXPORT const unsigned char DCKeychainItemWrapperVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <DCKeychainItemWrapper/PublicHeader.h>
 
-#import <DCKeychainItemWrapper/DCKeychainItemWrapper.h>
+#import <KeychainItemWrapper/DCKeychainItemWrapper.h>
 
